51d2344f2dd25dd8ace67e13c18a6a171078722d
khadija-mahdi <khadijamahdi6@gmail.com>
2024-04-28 15:09:21 +0100

    add bootsrap

--- src/nextjs-frontend/src/app/ui/dashboard/home/content_area/crouseAnnounce.tsx
+++ src/nextjs-frontend/src/app/ui/dashboard/home/content_area/crouseAnnounce.tsx
@@ -1,35 +1,38 @@
+'use client'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Carousel from 'react-bootstrap/Carousel';
+import { Carousel } from 'react-bootstrap';
+
 import Image from 'next/image';
 
 function CarouselAnnounce() {
-  return (
-    <Carousel fade>
-      <Carousel.Item>
-        <Image className="" src='/assets/images/Valorant.png' alt="Profile Image" width={73} height={73}/>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image className="" src='/assets/images/Valorant.png' alt="Profile Image" width={73} height={73}/>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Image className="" src='/assets/images/Valorant.png' alt="Profile Image" width={73} height={73}/>
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
+	return (
+		<div>
+			<div id="carouselExampleControlsNoTouching" className="carousel slide" data-bs-touch="false">
+				<div className="carousel-inner">
+					<div className="carousel-item active">
+						{/* <Image src='/assets/images/Valorant.png' alt="Profile Image" width={363.08} height={544.63} /> */}
+
+					</div>
+					<div className="carousel-item">
+						{/* <Image src='/assets/images/Valorant.png' alt="Profile Image" width={363.08} height={544.63} /> */}
+
+					</div>
+					<div className="carousel-item">
+						{/* <Image src='/assets/images/Valorant.png' alt="Profile Image" width={363.08} height={544.63} /> */}
+
+					</div>
+				</div>
+				<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span className="visually-hidden">Previous</span>
+				</button>
+				<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+					<span className="carousel-control-next-icon" aria-hidden="true"></span>
+					<span className="visually-hidden">Next</span>
+				</button>
+			</div>
+		</div>
+	);
 }
 
 export default CarouselAnnounce;
--- src/nextjs-frontend/src/app/ui/dashboard/home/newTournament.tsx
+++ src/nextjs-frontend/src/app/ui/dashboard/home/newTournament.tsx
@@ -3,7 +3,7 @@ import CarouselAnnounce from './content_area/crouseAnnounce';
 
 function NewTournaments() {
     return (
-        < div className=""  style={{ padding: '20px', borderRadius: '8px' }}>
+        < div >
 			<CarouselAnnounce/>
         </div>
     );
