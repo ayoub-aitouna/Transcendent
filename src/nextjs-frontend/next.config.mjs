e69b2a51dc9e89f775b93122aa82e09b9f3ea936
khadija-mahdi <khadijamahdi6@gmail.com>
2024-04-28 01:49:02 +0100

    changes in home page

--- src/nextjs-frontend/next.config.mjs
+++ src/nextjs-frontend/next.config.mjs
@@ -1,17 +1,4 @@
-// next.config.mjs
-const nextConfig = {
-	// Enable CSS support
-	css: {
-	  // Enable CSS modules
-	  modules: true,
-	  // Enable automatic vendor prefixing
-	  // You can omit this if you're not using any CSS that requires vendor prefixing
-	  loaderOptions: {
-		// Enable autoprefixer
-		autoprefixer: true,
-	  },
-	},
-  };
-  
-  export default nextConfig;
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
